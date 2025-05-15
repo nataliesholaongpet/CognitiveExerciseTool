629f2f40e4f0cb66011453148b6fb4a85c759421
nataliesholaongpet <s5421494@bournemouth.ac.uk>
2025-05-15 03:06:38 +0100

    Added web push implementation

--- app.js
+++ app.js
@@ -163,12 +163,35 @@ function LifestyleTips() {
 
 
     }
-)}
+    )
+}
 
 document.getElementById('saveReminder').addEventListener('click', () => {
-        if (!("Notification" in window)) {
+    if (!("Notification" in window)) {
         alert("This system does not support push notifications");
         return;
+    }
+
+    async function subscribeUserToPush() {
+        const registration = await navigator.serviceWorker.ready;
+
+        const subscription = await registration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: urlBase64ToUint8Array('YOUR_PUBLIC_VAPID_KEY')
+        });
+
+        console.log('Push Subscription:', subscription);
+    }
+
+    function urlBase64ToUint8Array(base64String) {
+        const padding = '='.repeat((4 - base64String.length % 4) % 4);
+        const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
+        const rawData = window.atob(base64);
+        const outputArray = new Uint8Array(rawData.length);
+        for (let i = 0; i < rawData.length; ++i) {
+            outputArray[i] = rawData.charCodeAt(i);
+        }
+        return outputArray;
     }
 
     if (Notification.permission === "granted") {
@@ -177,6 +200,7 @@ document.getElementById('saveReminder').addEventListener('click', () => {
         Notification.requestPermission().then(permission => {
             if (permission === "granted") {
                 new Notification("Notifications enabled!");
+                subscribeUserToPush();
             } else {
                 alert("Notifications denied.");
             }
